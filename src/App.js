c13afcc7c6dda575674f1fbd305015fe4df91f8e
AkoruuDev <akoruu.dev@gmail.com>
2024-02-16 00:20:01 -0300

    feat: connect pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About, Contact, Home, NotFound } from "./pages";
+
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+          {/* <Route path="/about/ac/:id" element={<Team />} /> */}
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+
+          {/* <Route path="/systems" element={<MenuItems />} /> */}
+          {/* <Route path="/universe" element={<MenuItems />} /> */}
+
+          {/* <Route path="/blog" element={<Blog />} /> */}
+          {/* <Route path="/post/:id" element={<Post />} /> */}
+          {/* <Route path="/chat" element={<Chat />} /> */}
+
+          {/* <Route path="/store" element={<Store />} /> */}
+          {/* <Route path="/product/:name" element={<ProductDetails />} /> */}
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
