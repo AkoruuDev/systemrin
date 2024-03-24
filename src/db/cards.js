713734681277cc0549d46fde1bbaa3df9303c4da
AkoruuDev <akoruu.dev@gmail.com>
2024-03-24 14:01:45 -0300

    db: add card model

--- src/db/cards.js
+++ src/db/cards.js
@@ -1,17 +1,19 @@
 import { colors } from "./colors";
 
 const cards = [
-    {
+    /* Model default*/
+    /* {
         id: 0,
         title: "Title",
-        description: "This is the card description",
-        imageUrl:"https://github.com/AkoruuDev/systemrin/blob/main/src/assets/default-image.jpg?raw=true",
-        type: 'System',
+        description: "The description about this card must content 50 caracters",
+        imageUrl: "https://github.com/AkoruuDev/systemrin/blob/main/src/assets/default-image.jpg?raw=true",
+        type: 'System', //'System' or 'Universe
         type_box: {
-            name: 'type name',
-            color: colors.purple
+            name: 'Card theme',
+            color: colors.orange
         }
-    },
+    }, */
+    /* ------------------------------ */
 ];
 
 export {
