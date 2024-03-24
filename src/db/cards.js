753a0821b612c27253f3217590c1cf6d5a6a2a24
AkoruuDev <akoruu.dev@gmail.com>
2024-03-24 14:02:18 -0300

    db: add level progression

--- src/db/cards.js
+++ src/db/cards.js
@@ -14,6 +14,17 @@ const cards = [
         }
     }, */
     /* ------------------------------ */
+    {
+        id: 1,
+        title: "Level progression",
+        description: "Accumulate XP Level (XN) to advance in level in Rinhuix. When you level up, transform XN into Cash XP (XC) to improve your record. XC is essential for attributes and powers. Spend wisely, as XC can only be used when leveling up.",
+        imageUrl:"https://images.playground.com/f7edf01022884fb18d9c1f953a30f899.jpeg",
+        type: 'System',
+        type_box: {
+            name: 'Game machanics',
+            color: colors.orange
+        }
+    },
 ];
 
 export {
