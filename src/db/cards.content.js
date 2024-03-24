e4651ef23e6c4c05f8edf8436280f10b6f94151b
AkoruuDev <akoruu.dev@gmail.com>
2024-03-24 13:56:55 -0300

    db: add level progression content

---
+++ src/db/cards.content.js
@@ -0,0 +1,258 @@
+const cards_content = [
+    {
+        card_id: 1,
+        content: [
+            {
+                type: "title",
+                content: "Progressão de Nível"
+            },{
+                type: "paragraph",
+                content: "XP Nível (XN) é aquilo que você acumula para passar de nível, pode ser obtido em treinos, missões, tarefas, estudos e etc."
+            },{
+                type: "paragraph",
+                content: "Quando você passa de nível, todos os seus pontos XN são transformado em XP Caixa (XC). Zerando o XN mais uma vez."
+            },{
+                type: "paragraph",
+                content: "Do nível 00 ao nível 01, é necessário obter 100 XN. Caso você tenha por exemplo 90 de XN e ganhe mais 15, os 5 extra são os adicionados ao XN do próximo nível, onde é necessário 200 XN pra passar."
+            },{
+                type: "paragraph",
+                content: "XC é aquele que é usado para comprar coisas em sua ficha. Como atributos, pontos de vida, slots power e tudo mais. Basicamente, toda sua evolução depende do XC."
+            },{
+                type: "paragraph",
+                content: "É possível acumular XC, mas ele só pode ser gasto única e exclusivamente quando sobe de nível. O XC só pode ser obtido subindo de nível, por isso quando subir vai obter mais, porém aquele gasto não vai retornar."
+            },{
+                type: "subtitle",
+                content: "Barras de XP"
+            },{
+                type: "table",
+                content: [
+                    {
+                        type: "title",
+                        content: [
+                            "Level",
+                            "XP"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 00",
+                            "000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 01",
+                            "100"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 02",
+                            "200"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 03",
+                            "300"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 04",
+                            "400"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 05",
+                            "500"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 06",
+                            "600"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 07",
+                            "700"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 08",
+                            "800"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 09",
+                            "900"
+                        ]
+                    },
+                ]
+            },{
+                type: "quote",
+                content: "(Total: 4.500)"
+            },{
+                type: "table",
+                content: [
+                    {
+                        type: "title",
+                        content: [
+                            "Level",
+                            "XP"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 10",
+                            "5.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 11",
+                            "9.500"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 12",
+                            "15.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 13",
+                            "20.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 14",
+                            "25.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 15",
+                            "30.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 16",
+                            "35.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 17",
+                            "40.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 18",
+                            "45.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 19",
+                            "50.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 20",
+                            "60.000"
+                        ]
+                    },
+                ]
+            },{
+                type: "quote",
+                content: "(Total: 339.000)"
+            },{
+                type: "quote",
+                content: "Obs: Nível 20 é o máximo normal, mas após ele você estará nos chamados níveis fantasmas e assim sua evolução continua de forma diferenciada."
+            },{
+                type: "table",
+                content: [
+                    {
+                        type: "title",
+                        content: [
+                            "Level",
+                            "XP"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 21 - 49",
+                            "77.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 50 - 99",
+                            "85.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 100 - 199",
+                            "100.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 200 - 249",
+                            "260.000"
+                        ]
+                    },{
+                        type: "content",
+                        content: [
+                            "level 250",
+                            "700.000"
+                        ]
+                    },
+                ]
+            },{
+                type: "subtitle",
+                content: "Habilities"
+            },{
+                type: "paragraph",
+                content: "Por nivel, você recebe benefícios e em alguns niveis especificos é possível desbloquear habilidades naturais de um Pohuvulduh"
+            },{
+                type: "list",
+                content: "A partir do nível 25, é possivel mudar a composição de seu corpo, alterando pra qualquer aparência humanoide comum, mas sem poder extrapolar os limites de aparência fisica de um civil, mas pode alternar externa e internamente o corpo pra fêmea, macho, um híbrido de ambos ou nenhum dos dois. O processo todo leva de 1 semana a 1 Mês."
+            },{
+                type: "list",
+                content: "A partir do nível 30, tem seus limites de atributos, Vida, Sanidade, estamina, Movimento e iniciativa dobrados automaticamente."
+            },{
+                type: "list",
+                content: "A partir do Nível 50, é possivel despertar o estágio Alfa na Ordem dos seus poderes, além de poder se tornar um Deus e se morrer antes disso, vira uma entidade espiritual ou paranormal de médio nível."
+            },{
+                type: "list",
+                content: "No nível 100, é possível despertar o estágio Ômega na Ordem dos seus poderes, Além de superar o estágio de Deus e se morrer antes disso, vira uma entidade espiritual ou paranormal de alto nível."
+            },{
+                type: "list",
+                content: "No nível 150, é possível despertar o estágio Delta na Ordem dos seus poderes além de poder se tornar digno de uma Jóia Deva"
+            },{
+                type: "list",
+                content: "No nível 200, é possível despertar o estágio Deva na Ordem dos seus poderes, além de poder manifestar entidades espirituais ou paranormais a partir do seu corpo e continuar vivo, mas essas entidades são de baixo nível e cabe a elas evoluirem."
+            },{
+                type: "list",
+                content: "A partir do nível 250, é possivel abrir portais pra outros planos, mas sempre sujeito as leis daquele plano. Também pode manipular com menos dificuldade, as Forças Inter Planos."
+            },
+        ]
+    },
+]
+
+export {
+    cards_content
+}
