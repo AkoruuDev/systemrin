0cc3dfa8ad9d559b54f4a6af5fa3647c9d183ea1
Akoruu Dev <akoruu.dev@gmail.com>
2024-04-28 23:51:27 -0300

    chore: update voice page

--- src/content/Voices.js
+++ src/content/Voices.js
@@ -2,10 +2,19 @@
 https://rinwiki.vercel.app/universe/aux/voice/Um%20exemplo%20disso%20que%20eu%20poderia%20dar%20a%20voc%C3%AA,%20leitor,%20seriam%20os%20m%C3%A9diuns%20e%20pensadores%20que%20transcenderam%20suas%20%C3%A9pocas.
 */
 
-import { useParams } from "react-router-dom"
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom"
 
 function Voice() {
     const {voiceTxt} = useParams();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log("Vai filho")
+        window.location.href = `https:youtube.com/watch?v=${voiceTxt}`
+        console.log("FOi não")
+    }, [])
     
     return (
         <>{voiceTxt}</>
